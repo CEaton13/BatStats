df7848ab8887e2c0ac480e14397673fa5807d11b
Charles Eaton <cre.1323@gmail.com>
2025-12-01 16:48:38 -0600

    Initializing Spring Maven Project with 4 basic dependencies

---
+++ src/main/java/com/skillstormproject1/batstats/BatstatsApplication.java
@@ -0,0 +1,13 @@
+package com.skillstormproject1.batstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatstatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatstatsApplication.class, args);
+	}
+
+}
