81e17632374ae3e1f48a82d47b2ae1a30b4c9b67
Charles Eaton <32652102+CEaton13@users.noreply.github.com>
2025-12-06 16:20:50 -0600

    Commented Repos to start with services

--- src/main/java/com/skillstormproject1/batstats/repositories/InventoryItemRepository.java
+++ src/main/java/com/skillstormproject1/batstats/repositories/InventoryItemRepository.java
@@ -1,8 +1,20 @@
 package com.skillstormproject1.batstats.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface InventoryItemRepository {
+import com.skillstormproject1.batstats.models.InventoryItem;
+
+@Repository     // Repository Interface for InventoryItems
+public interface InventoryItemRepository extends JpaRepository<InventoryItem, Integer> {
+
+    /**
+     *  Services
+     *      findBySerialNum
+     *      findByWarehouse
+     *      findByProductType
+     *      existsSerialNum
+     *      search
+     */
 
 }
--- src/main/java/com/skillstormproject1/batstats/repositories/ProductTypeRepository.java
+++ src/main/java/com/skillstormproject1/batstats/repositories/ProductTypeRepository.java
@@ -1,8 +1,16 @@
 package com.skillstormproject1.batstats.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ProductTypeRepository {
+import com.skillstormproject1.batstats.models.ProductType;
 
+@Repository     // Repository Interface for ProductType
+public interface ProductTypeRepository extends JpaRepository<ProductType, Integer> {
+    /**
+     *  Services are going to depend on the Repos
+     *      findByName
+     *      findBy*
+     *      check if a product exists
+     */
 }
