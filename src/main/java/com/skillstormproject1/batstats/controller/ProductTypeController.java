95a8838b4fbb8a8eac9434a7c23302ef65051e76
Charles Eaton <cre.1323@gmail.com>
2025-12-08 00:48:43 -0600

    deleted some unused imports from moving around code

--- src/main/java/com/skillstormproject1/batstats/controller/ProductTypeController.java
+++ src/main/java/com/skillstormproject1/batstats/controller/ProductTypeController.java
@@ -7,12 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.skillstormproject1.batstats.models.InventoryItem;
+
 import com.skillstormproject1.batstats.models.ProductType;
-import com.skillstormproject1.batstats.services.InventoryItemService;
 import com.skillstormproject1.batstats.services.ProductTypeService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
