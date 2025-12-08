11c35366a30b3301c471df1beb7e20dbece7cf5f
Charles Eaton <cre.1323@gmail.com>
2025-12-08 00:46:46 -0600

    @GetMapping updates

--- src/main/java/com/skillstormproject1/batstats/controller/WarehouseController.java
+++ src/main/java/com/skillstormproject1/batstats/controller/WarehouseController.java
@@ -2,7 +2,10 @@ package com.skillstormproject1.batstats.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skillstormproject1.batstats.models.Warehouse;
 import com.skillstormproject1.batstats.services.WarehouseService;
+
+import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,5 +22,30 @@ public class WarehouseController {
         this.warehouseService = warehouseService;
     }
 
+    // get a list of all warehouses
+    @GetMapping
+    public ResponseEntity<List<Warehouse>> getAllWarehouses() {
+        return ResponseEntity.ok(warehouseService.getAllWarehouses());
+    }
 
+    // get a warehouse by its id
+    @GetMapping("/{id}")
+    public ResponseEntity<Warehouse> getWarehouseById(@PathVariable int id) {
+        return ResponseEntity.ok(warehouseService.getWarehouseById(id));
+    }
+
+    // get all warehouses by a specific status
+    @GetMapping("/status/{status}")
+    public ResponseEntity<List<Warehouse>> getWarehousesByStatus(@PathVariable String status) {
+        return ResponseEntity.ok(warehouseService.getWarehouseByStatus(status));
+    }
+
+
+    /**
+     * createWarehouse PostMapping
+     * updateWarehouse PutMapping
+     * deleteWarehouse DeleteMapping
+     */
+
+    
 }
